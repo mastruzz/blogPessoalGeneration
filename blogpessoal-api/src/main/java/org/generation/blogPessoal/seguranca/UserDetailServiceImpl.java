6918c0119bcd9c529c6f6aad4e93188f03cc1394
mastruzz <72388859+mastruzz@users.noreply.github.com>
2020-11-18 12:30:08 -0300

    deploy do blog pessoal

--- blogpessoal-api/src/main/java/org/generation/blogPessoal/seguranca/UserDetailServiceImpl.java
+++ blogpessoal-api/src/main/java/org/generation/blogPessoal/seguranca/UserDetailServiceImpl.java
@@ -2,8 +2,10 @@ package org.generation.blogPessoal.seguranca;
 
 import java.util.Optional;
 
+
 import org.generation.blogPessoal.model.Usuario;
 import org.generation.blogPessoal.repository.UsuarioRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
--- blogpessoal-api/src/main/java/org/generation/blogPessoal/seguranca/UserDetailsImpl.java
+++ blogpessoal-api/src/main/java/org/generation/blogPessoal/seguranca/UserDetailsImpl.java
@@ -9,12 +9,17 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class UserDetailsImpl implements UserDetails {
 
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private String userName;
 	
 	private String password;
 	
 	public UserDetailsImpl(Usuario user) {
-		this.userName = user.getUsuario();
+		this.userName = user.getNome();
 		this.password = user.getSenha();
 		
 	}
